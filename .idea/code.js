28025bc4d8452838bc06a072037f3498cbf646ca
abecediaz <abecediaz@gmail.com>
2025-03-21 16:30:23 -0300

    Creación y desarrollo del archivo register.html

--- .idea/code.js
+++ .idea/code.js
@@ -1,6 +1,7 @@
 function doGet(e) {
-    return HtmlService.createHtmlOutputFromFile("login")
-        .setTitle("Sistema de Horas Extraordinarias | Login")
+    let page = e.parameter.page || "login";
+    return HtmlService.createHtmlOutputFromFile(page)
+        .setTitle("Sistema Interno de Horas Extraordinarias")
 }
 
 function obtenerInformacionUsuarios() {
@@ -13,4 +14,8 @@ function hashPassword(password) {
     return hash.map(function(byte) {
         return ('0' + (byte & 0xFF).toString(16)).slice(-2);
     }).join('');
+}
+
+function cargarUsuarioNuevo(id, usuario, email, password) {
+    SpreadsheetApp.getActiveSpreadsheet().getSheetByName("USERS").appendRow([id, usuario, email, password, "", "", ""]);
 }
